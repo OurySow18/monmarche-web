17f191e577b92d3ad92904fbf05d6e093e3ef5b2
Amadou Oury Sow <aos@abatgroup.de>
2026-01-27 21:43:09 +0100

    Add blog content and update config

--- functions/next.config.js
+++ functions/next.config.js
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone', 
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "monmarchegn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/apple-app-site-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json"
+          }
+        ]
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;
